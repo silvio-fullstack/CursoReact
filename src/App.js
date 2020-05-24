2133babf22263197119c2ab975575313738b7cf4
silvio-fullstack <sbalves@bol.com.br>
2020-05-24 02:00:36 -0300

    Quarto Comit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
- 
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import Cabecalho from './componentes/Cabecalho'
-import Site from './Site'
+import Inicio from './componentes/Inicio'
+import Servico from './componentes/Servicos'
 import Rodape from './componentes/Rodape'
+import Portifolio from './componentes/Portifolio'
+import Precos from './componentes/Precos'
+import Contato from './componentes/Contato'
 
 function App() {
   return (
-    <div className="App">
-      <Cabecalho />
-      <Site /> 
-      <Rodape />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Cabecalho />
+
+        <Route path='/' exact component={Inicio}/>
+        <Route path='/servicos' component={Servico}/>
+        <Route path='/portifolio' component={Portifolio}/>
+        <Route path='/precos' component={Precos}/>
+        <Route path='/contato' component={Contato}/>
+  
+        <Rodape />
+      </div></BrowserRouter>
+
   );
 }
 
--- src/componentes/Cabecalho.js
+++ src/componentes/Cabecalho.js
@@ -1,45 +1,69 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Cabecalho = (props) => {
   return (
-    <nav className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#myNavbar"
-          >
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="#myPage">
-            Logo
-          </a>
-        </div>
-        <div className="collapse navbar-collapse" id="myNavbar">
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="#about">ABOUT</a>
-            </li>
-            <li>
-              <a href="#services">SERVICES</a>
-            </li>
-            <li>
-              <a href="#portfolio">PORTFOLIO</a>
-            </li>
-            <li>
-              <a href="#pricing">PRICING</a>
-            </li>
-            <li>
-              <a href="#contact">CONTACT</a>
-            </li>
-          </ul>
-        </div>
+    <div>
+      <div className="jumbotron text-center">
+        <h1>Company</h1>
+        <p>We specialize in blablabla</p>
+        <form>
+          <div className="input-group">
+            <input
+              type="email"
+              className="form-control"
+              size="50"
+              placeholder="Email Address"
+              required
+            />
+            <div className="input-group-btn">
+              <button type="button" className="btn btn-danger">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </nav>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#myNavbar"
+            >
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <Link className="navbar-brand" to='/'>
+              Logo
+          </Link>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link to ='/'>INICIO</Link>
+              </li>
+              <li>
+                <Link to ='/servicos'>SERVIÇOS</Link>
+              </li>
+              <li>
+                <Link to = '/portifolio'>PORTIFOLIO</Link>
+              </li>
+              <li>
+                <Link to ='/precos'>VALORES</Link>
+              </li>
+              <li>
+                <Link to ='/contato'>FALE CONOSCO</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
   );
 };
 
