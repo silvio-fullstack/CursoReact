d642b7a700040dc02df65edc227f375a745d0a07
silvio-fullstack <sbalves@bol.com.br>
2020-05-23 21:15:56 -0300

    terceiro comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
  
+import Cabecalho from './componentes/Cabecalho'
 import Site from './Site'
 import Rodape from './componentes/Rodape'
 
 function App() {
   return (
     <div className="App">
+      <Cabecalho />
       <Site /> 
       <Rodape />
     </div>
--- src/Site.js
+++ src/Site.js
@@ -3,45 +3,6 @@ import React from "react";
 const Site = (props) => {
   return (
     <div>
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#myNavbar"
-            >
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#myPage">
-              Logo
-            </a>
-          </div>
-          <div className="collapse navbar-collapse" id="myNavbar">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#about">ABOUT</a>
-              </li>
-              <li>
-                <a href="#services">SERVICES</a>
-              </li>
-              <li>
-                <a href="#portfolio">PORTFOLIO</a>
-              </li>
-              <li>
-                <a href="#pricing">PRICING</a>
-              </li>
-              <li>
-                <a href="#contact">CONTACT</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <div className="jumbotron text-center">
         <h1>Company</h1>
         <p>We specialize in blablabla</p>
